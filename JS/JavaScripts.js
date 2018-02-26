38d34f35d527d1400f0145a22fd3cf3afc193f40
liamgreig1 <liam.greig1998@googlemail.com>
2018-02-26 20:34:22 +0000

    Add files via upload

---
+++ JS/JavaScripts.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function load_cCategory() {
+     document.getElementById("category").innerHTML='<object type="text/html" data="../HTML/CreateCategory.html" ></object>';
+}
+
+function load_aQuestion() {
+     document.getElementById("category").innerHTML='<object type="text/html" data="../HTML/AddQuestion.html" ></object>';
+}
+
+function load_dCategory() {
+     document.getElementById("category").innerHTML='<object type="text/html" data="../HTML/DeleteCategory.html" ></object>';
+}
+
+function load_dQuestion() {
+     document.getElementById("category").innerHTML='<object type="text/html" data="../HTML/DeleteQuestion.html" ></object>';
+}
+
+function load_Master() {
+     document.getElementById("head").innerHTML='<object type="text/html" data="../HTML/Master.html" ></object>';
+}
