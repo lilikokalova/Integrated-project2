7a57e042cf6a84cca44245b2c321321682d13872
elMerin <hmerin@gmail.com>
2018-04-08 17:48:43 +0100

    Add files via upload

---
+++ SQL/quizscore.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 08, 2018 at 06:45 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fdmquiz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizscore`
+--
+
+CREATE TABLE `quizscore` (
+  `quizID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `categoryID` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `quizscore`
+--
+ALTER TABLE `quizscore`
+  ADD PRIMARY KEY (`quizID`),
+  ADD KEY `quizscore_ibfk_1` (`userID`),
+  ADD KEY `quizscore_ibfk_2` (`categoryID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `quizscore`
+--
+ALTER TABLE `quizscore`
+  MODIFY `quizID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `quizscore`
+--
+ALTER TABLE `quizscore`
+  ADD CONSTRAINT `quizscore_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `quizscore_ibfk_2` FOREIGN KEY (`CategoryID`) REFERENCES `category` (`categoryID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
