a7b0d8ee1e8a6d99bc0364b8583d31228be03b09
elMerin <hmerin@gmail.com>
2018-04-12 20:04:11 +0100

    Update setScore.php

--- PHP/setScore.php
+++ PHP/setScore.php
@@ -2,7 +2,37 @@
 require 'dbConnect.php';
 session_start();
 
-header("location: ../PHP/Homepage.php");
+$score = 0;
+
+	$numQuestions = $_POST['numQuestions'];
 	
 	
+	for($x = 1; $x <= $numQuestions; $x++){
+		
+			if( isset($_POST['q'.$x]) ){
+			
+				$cName = $_POST['q'.$x];
+				$result = $mysqli->query("SELECT * FROM choice WHERE choiceName = '$cName'");
+				$c = $result->fetch_assoc();
+				
+				
+				
+				if($c['correct']){
+					$score++;
+				}
+			}
+
+	}
+	
+	$score = ($score/$numQuestions)*100;
+	$uName = $_SESSION['uname'];
+	$result = $mysqli->query("SELECT * FROM user WHERE userName = '$uName'");
+	$userID = $result->fetch_assoc();
+	$userID = $userID['userID'];
+	$catID = $_POST['catID'];
+	
+	$result = $mysqli->query("INSERT INTO quizScore(userID, categoryID, score, date) 
+	VALUES($userID,$catID,$score,now())");
+		
+	header("location: ../PHP/Homepage.php");	
 ?>
