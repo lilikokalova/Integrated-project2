33db284696758aac9ce5a71ea076a1740879e579
liamgreig1 <liam.greig1998@googlemail.com>
2018-04-12 14:43:23 +0100

    Add files via upload

--- PHP/DropCategory.php
+++ PHP/DropCategory.php
@@ -1,17 +1,7 @@
 <?php
 require 'dbConnect.php';
 session_start();
-
 $cat = $_POST['Categories'];
-
-$query="DELETE FROM category WHERE categoryName = '$cat'";
-
-if(mysqli_query($mysqli, $query)){
-    echo "Records were deleted successfully.";
-} else{
-    echo "ERROR: Could not able to execute $query. " . mysqli_error($mysqli);
-}
-
-	header("location: ../PHP/CreateCategory.php");
-}
+$result = $mysqli_query->query("DELETE FROM category WHERE categoryName = '$cat'");
+header("location: ../PHP/CreateCategory.php");
 ?>
